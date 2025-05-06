2ade18f11df5189c1d7f5289607b719daf277d8b
koalazf99 <koala99.zf@gmail.com>
2025-05-06 09:17:55 +0000

    small fix

--- scripts/offline_eval/cruxeval.sh
+++ scripts/offline_eval/cruxeval.sh
@@ -11,23 +11,23 @@ for dataset in ${datasets[@]}; do
     data_path=./data/test/${dataset}.parquet
     model_name=$(basename $model_path)
     save_path=./data/test/${dataset}_${model_name}_gen.parquet
-    # # === Generation ===
-    # echo "Generating with $model_name on ${dataset}..."
-    # python3 -m verl.trainer.main_generation \
-    #     trainer.nnodes=1 \
-    #     trainer.n_gpus_per_node=8 \
-    #     data.path=${data_path} \
-    #     data.prompt_key=prompt \
-    #     data.n_samples=1 \
-    #     data.output_path=${save_path} \
-    #     model.path=${model_path} \
-    #     rollout.temperature=0.6 \
-    #     rollout.top_k=50 \
-    #     rollout.top_p=0.95 \
-    #     rollout.prompt_length=4096 \
-    #     rollout.response_length=4096 \
-    #     rollout.tensor_model_parallel_size=2 \
-    #     rollout.gpu_memory_utilization=0.8
+    # === Generation ===
+    echo "Generating with $model_name on ${dataset}..."
+    python3 -m verl.trainer.main_generation \
+        trainer.nnodes=1 \
+        trainer.n_gpus_per_node=8 \
+        data.path=${data_path} \
+        data.prompt_key=prompt \
+        data.n_samples=1 \
+        data.output_path=${save_path} \
+        model.path=${model_path} \
+        rollout.temperature=0.6 \
+        rollout.top_k=50 \
+        rollout.top_p=0.95 \
+        rollout.prompt_length=4096 \
+        rollout.response_length=4096 \
+        rollout.tensor_model_parallel_size=2 \
+        rollout.gpu_memory_utilization=0.8
 
     # === Evaluation ===
     echo "Evaluating with $model_name on ${dataset}..."
