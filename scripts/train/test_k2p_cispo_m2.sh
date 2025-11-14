99c7c65203edebe59b9e2e4c4d2abdbba89be6c6
Taylor Killian <twkillian@gmail.com>
2025-11-14 08:16:17 +0000

    sync

--- scripts/train/test_k2p_cispo_m2.sh
+++ scripts/train/test_k2p_cispo_m2.sh
@@ -15,7 +15,7 @@
 
 
 # =================== Frequently Used Variables ===================
-RESUME_CKPT_DIR_NAME=""  # Fill in the checkpoint directory name to resume from, otherwise from scratch
+RESUME_CKPT_DIR_NAME="cispo-focused-fixed-Qwen2.5-7B-1016971"  # Fill in the checkpoint directory name to resume from, otherwise from scratch
 export STEM_LLM_JUDGE_URL="http://10.24.2.1:8000"  # Fill in the llm-as-judge hosted URL, currently used only in 'STEM' domain
 
 # =================== Cluster Environment ===================
