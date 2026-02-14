2f0dc5bfdc951f9eb2afbf03119dd4617baa24bf
Taylor Killian <twkillian@gmail.com>
2026-02-14 00:01:28 +0000

    sync

--- scripts/train/k2p_hero_grpo_newData.sh
+++ scripts/train/k2p_hero_grpo_newData.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=grpo-stage2-k2pRL-easy50k-7domains
-#SBATCH --nodes=64
-#SBATCH --ntasks=64
+#SBATCH --job-name=grpo-stage1-k2pRL-mainMathOnly
+#SBATCH --nodes=32
+#SBATCH --ntasks=32
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:8
 #SBATCH --cpus-per-task=96
@@ -14,7 +14,7 @@
 # commenting out... SBATCH --exclude=azure-uk-hpc-H200-instance-[043-060,249,347-410]
 
 # =================== Frequently Used Variables ===================
-RESUME_CKPT_DIR_NAME="grpo-stage2-k2pRL-easy50k-7domains-415354"  # Fill in the checkpoint directory name to resume from, otherwise from scratch
+RESUME_CKPT_DIR_NAME=""  # Fill in the checkpoint directory name to resume from, otherwise from scratch
 export STEM_LLM_JUDGE_URL="http://azure-uk-hpc-H200-instance-004:8000" # Fill in the llm-as-judge hosted URL, currently used only in 'STEM' domain
 export MATH_LLM_JUDGE_URL="http://azure-uk-hpc-H200-instance-284:8000" # Fill in the OmniMATH llm-as-judge hosted URL, only used to score OmniMATH dataset if not empty
 
@@ -71,19 +71,19 @@ iq400_path="/lustrefs/users/taylor.killian/Reasoning360/data/guru_data/iq400_pro
 # List of datasets to include (filename only)
 # Comment out lines to exclude specific datasets
 dataset_names=(
-    "codegen__deduped_leetcode2k_2.4k.parquet"
-    "codegen__deduped_livecodebench_599.parquet"
-    "codegen__deduped_primeintellect_9.6k.parquet"
-    "codegen__deduped_taco_11.1k.parquet"
-    "ifbench__fixed_85.6k.parquet"
     "math__combined_118.2k.part1.parquet"
     "math__combined_118.2k.part2.parquet"
     "omni_math_4.43k_dedup.parquet"
-    "stem__nemotron_13.3k.parquet"
-    "stem__web_31.7k.parquet"
-    "table__hitab_7.4k.parquet"
-    "table__multihier_2.9k.parquet"
 )
+    # "stem__nemotron_13.3k.parquet"
+    # "stem__web_31.7k.parquet"
+    # "table__hitab_7.4k.parquet"
+    # "table__multihier_2.9k.parquet"
+# "codegen__deduped_leetcode2k_2.4k.parquet"
+#     "codegen__deduped_livecodebench_599.parquet"
+#     "codegen__deduped_primeintellect_9.6k.parquet"
+#     "codegen__deduped_taco_11.1k.parquet"
+#     "ifbench__fixed_85.6k.parquet"
 #     "simulation__codeio_fixed_12.1k.parquet"
 #     "logic__arcagi1_297.parquet"
 #     "logic__arcagi2_653.parquet"
@@ -166,14 +166,15 @@ if_bench_test_path=${TEST_DATA_DIR}/ifbench_800.parquet
 # test_files="['${math_test_path}','${aime_test_path}','${aime25_test_path2}','${amc_test_path}','${humaneval_test_path}','${mbpp_test_path}','${livecodebench_test_path}','${nemotron_test_path}','${gpqa_diamond_test_path}','${supergpqa_test_path}']"
 
 # Full data mixture (uncomment to use)
-test_files="['${math_test_path}','${aime_test_path}','${aime25_test_path2}','${amc_test_path}','${humaneval_test_path}','${mbpp_test_path}','${livecodebench_test_path}','${zebralogic_test_path}','${reasoninggym_test_path}','${arcagi1_test_path}','${multihier_test_path}','${hitab_test_path}','${nemotron_test_path}','${gpqa_diamond_test_path}','${supergpqa_test_path}','${if_test_path}','${if_bench_test_path}']" # ,'${iq400_path}', '${synlogic_test_path}',
+test_files="['${math_test_path}','${aime_test_path}','${aime25_test_path2}','${amc_test_path}']"
+# test_files="['${math_test_path}','${aime_test_path}','${aime25_test_path2}','${amc_test_path}','${humaneval_test_path}','${mbpp_test_path}','${livecodebench_test_path}','${zebralogic_test_path}','${reasoninggym_test_path}','${arcagi1_test_path}','${multihier_test_path}','${hitab_test_path}','${nemotron_test_path}','${gpqa_diamond_test_path}','${supergpqa_test_path}','${if_test_path}','${if_bench_test_path}']" # ,'${iq400_path}', '${synlogic_test_path}',
 
 
 # =================== Model ===================
 # BASE_MODEL=/lustrefs/users/runner/workspace/checkpoints/huggingface/sft/mid4_rope_sft_reasoning_am_251117/checkpoints/checkpoint_0002250  # AM-Think SFT
-# BASE_MODEL=/lustrefs/users/varad.pimpalkhute/data_process/K2-Plus-Oss-Instruct-mid4 # Final Instruct SFT (after stg4_iter 10k)
+BASE_MODEL=/lustrefs/users/varad.pimpalkhute/data_process/K2-Plus-Oss-Instruct-mid4 # Final Instruct SFT (after stg4_iter 10k)
 # BASE_MODEL=/lustrefs/users/varad.pimpalkhute/data_process/K2-Plus-Instruct-mid4 # Instruct SFT, after stg4_iter 7k
-BASE_MODEL=/lustrefs/users/taylor.killian/Reasoning360/checkpoints/k2plus_rl/grpo-k2p-newFiltered-32k-mainQs-finalInstruct-406955/global_step_330/actor/huggingface
+# BASE_MODEL=/lustrefs/users/taylor.killian/Reasoning360/checkpoints/k2plus_rl/grpo-k2p-newFiltered-32k-mainQs-finalInstruct-406955/global_step_330/actor/huggingface
 
 # =================== Logging ===================
 WANDB_PROJECT=k2plus_rl
@@ -228,7 +229,7 @@ clip_ratio_low=0.2
 clip_ratio_high=0.28
 
 max_prompt_length=$((1024 * 4))
-max_response_length=$((1024 * 64))
+max_response_length=$((1024 * 32))
 enable_overlong_buffer=False
 overlong_buffer_len=$((1024 * 12))
 overlong_penalty_factor=1.0
@@ -342,7 +343,7 @@ offload=True
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.model.enable_activation_offload=${offload} \
     actor_rollout_ref.model.use_liger=True \
-    reward_model.reward_manager=dapo \
+    reward_model.reward_manager=async_multi_process \
     reward_model.overlong_buffer.enable=${enable_overlong_buffer} \
     reward_model.overlong_buffer.len=${overlong_buffer_len} \
     reward_model.overlong_buffer.penalty_factor=${overlong_penalty_factor} \
